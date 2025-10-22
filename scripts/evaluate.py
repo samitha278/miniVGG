7f37d8a64c6dd44197cc5cace5c92d987af91142
samitha278 <samithasahanssb@gmail.com>
2025-10-22 22:58:12 +0530

    implemented evaluate.py to evaluate model on imagenet 100 val dataset

---
+++ scripts/evaluate.py
@@ -0,0 +1,34 @@
+import torch
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from src.models.vgg_v3 import minVGG,Config
+from src.evaluation.evaluation import evaluate
+
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+torch.manual_seed(278)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(278)
+
+# Load model
+config = Config()
+model = minVGG(config)
+
+# checkpoint = torch.load("path",map_location='cuda') 
+
+
+# model.load_state_dict(checkpoint)
+# model.to(device)
+
+# Load val data
+from src.data.dataset import get_val_dataloader
+val_loader,class_names = get_val_dataloader("/home/samitha/Projects/datasets/imagenet100",classes=True)
+
+model.eval()
+results = evaluate(model, val_loader, class_names,device)
+
+print(results)
